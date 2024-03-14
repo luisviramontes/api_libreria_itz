4129f06c5d3ce8aa702e9422953399886fc9fd37
Luis Viramontes <luis_alfonso133@hotmail.com>
2024-03-14 09:16:43 -0600

    DB

--- loadBalancer.js
+++ loadBalancer.js
@@ -4,12 +4,7 @@ const httpProxy = require('http-proxy');
 //Definimos una variable con las direcciones de las instancias de nuestra aplicacion
 
 const appServers=[
-    {host:'localhost',port:3001},
-    {host:'localhost',port:3002},
-    {host:'localhost',port:3003},
-    {host:'localhost',port:3004},
-    {host:'localhost',port:3005},
-    {host:'localhost',port:3006},
+    {host:'localhost',port:3001}
 ]
 
 //Crear un proxy
