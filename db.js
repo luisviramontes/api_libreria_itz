57e06bebad492bc014337cbb20630ef3e2b7e518
Luis Viramontes <luis_alfonso133@hotmail.com>
2024-03-13 19:18:00 -0600

    DB

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //incluimos Mongoose y abrimos una conexión
 var mongoose = require('mongoose')
-var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/db_librerias'
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://luisvr:zacatecas.8@docdb-2024-03-14-01-10-20.cluster-ctmucmi66m25.us-east-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
 mongoose.connect(MONGO_URL)
 
 mongoose.connection.on('connected',function(){
